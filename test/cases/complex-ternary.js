78ad15f52afa127a216faecfaa87f4fe4abb67c1
Le Vivilet <levivlet@pm.me>
2023-06-09 16:20:10 +0200

    test: add complex ternary test

---
+++ test/cases/complex-ternary.js
@@ -0,0 +1,13 @@
+const en = () =>
+  Wn ||
+  (Wn =
+    typeof globalThis < "u"
+      ? globalThis
+      : typeof self < "u"
+      ? self
+      : typeof window < "u"
+      ? window
+      : typeof global < "u"
+      ? global
+      : {});
+function something(e) {}
