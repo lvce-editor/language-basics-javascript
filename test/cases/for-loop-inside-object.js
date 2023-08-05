42825e5b4f614379433464db674ccb318ed01c51
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-08-05 16:15:17 +0200

    test: add more unusual property name tests (#123)

---
+++ test/cases/for-loop-inside-object.js
@@ -0,0 +1,5 @@
+const object = {
+  for(let i=0;i<10;i++){
+    this.value += i
+  }
+}
---
+++ test/cases/switch-case-inside-object.js
@@ -0,0 +1,9 @@
+const object ={
+  switch (value){
+    case 1:
+    case 2:
+      break
+    default:
+      break
+  }
+}
