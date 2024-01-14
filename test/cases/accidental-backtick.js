fac31a09d8c39b020aed16cabbe395383a22c30d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-14 11:33:01 +0100

    test: add backtick test (#141)

---
+++ test/cases/accidental-backtick.js
@@ -0,0 +1,7 @@
+export const editorO`penColorPicker = () => {
+  return I18nString.i18nString(UiStrings.EditorOpenColorPicker)
+}
+
+export const editorCloseColorPicker = () => {
+  return I18nString.i18nString(UiStrings.EditorCloseColorPicker)
+}
